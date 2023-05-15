cf2ab25d54885c4b30e56feff68cd484df918f5d
Oskars Braucs <oskars.braucs@gmail.com>
2023-05-15 11:32:49 +0300

    A test

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -9,4 +9,26 @@ class Stock < ApplicationRecord
       nil
     end
   end
+
+  def self.company_lookup(ticker_symbol)
+    response = HTTParty.get("https://www.alphavantage.co/query?function=OVERVIEW&symbol=#{ticker_symbol}&apikey=#{Rails.application.credentials.alpha_client[:UKVHCOE3YNKFYIZF]}")
+    if response.code == 200
+      response.parsed_response['Name']
+    else
+      nil
+    end
+  end
+
+# def self.new_lookup(ticker_symbol)
+#   response = HTTParty.get("https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=#{ticker_symbol}&apikey=#{Rails.application.credentials.alpha_client[:api_key]}")
+#   if response.code == 200 && response.parsed_response['Global Quote']
+#     last_price = response.parsed_response['Global Quote']['05. price']
+#     name = company_lookup(ticker_symbol)
+#     new(ticker: ticker_symbol, name: name, last_price: last_price)
+#   else
+#     nil
+#   end
+# end
+
+
 end
