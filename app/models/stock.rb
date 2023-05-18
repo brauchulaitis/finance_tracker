3c19b03ac459e3ae3c4738a7e6a296a6e65969aa
Oskars Braucs <oskars.braucs@gmail.com>
2023-05-18 14:18:25 +0300

    Dealing with invalid search results

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -21,12 +21,15 @@ class Stock < ApplicationRecord
 
   def self.new_lookup(ticker_symbol)
     response = HTTParty.get("https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=#{ticker_symbol}&apikey=#{Rails.application.credentials.alpha_client[:alpha_api_key]}")
+    begin
     if response.code == 200 && response.parsed_response['Global Quote']
       last_price = response.parsed_response['Global Quote']['05. price']
       name = company_lookup(ticker_symbol)
       new(ticker: ticker_symbol, name: name, last_price: last_price)
     else
-      nil
+      return nil
+    end
+      rescue => exception
     end
   end
 end
