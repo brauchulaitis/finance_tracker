6d2cd2dadca036152af49a2539af7eb7b4d9fc7d
Oskars Braucs <oskars.braucs@gmail.com>
2023-05-18 15:02:37 +0300

    Use Turbo-Data for form submission

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -22,14 +22,14 @@ class Stock < ApplicationRecord
   def self.new_lookup(ticker_symbol)
     response = HTTParty.get("https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=#{ticker_symbol}&apikey=#{Rails.application.credentials.alpha_client[:alpha_api_key]}")
     begin
-    if response.code == 200 && response.parsed_response['Global Quote']
-      last_price = response.parsed_response['Global Quote']['05. price']
-      name = company_lookup(ticker_symbol)
-      new(ticker: ticker_symbol, name: name, last_price: last_price)
-    else
-      return nil
-    end
-      rescue => exception
+      if response.code == 200 && response.parsed_response['Global Quote']
+        last_price = response.parsed_response['Global Quote']['05. price']
+        name = company_lookup(ticker_symbol)
+        new(ticker: ticker_symbol, name: name, last_price: last_price)
+      else
+        return nil
+      end
+    rescue => exception
     end
   end
 end
